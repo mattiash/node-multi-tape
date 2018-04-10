c7381b95a3ac2c186760e28812d8e394c291a822
LordScree <oliver.clare@gmail.com>
2018-04-11 00:53:49 +0100

    Added more tests and fixed issues.

--- index.js
+++ index.js
@@ -34,10 +34,16 @@ if(argv['node-arg']) {
 function globArgs(argv_) {
     let globbedFiles = []
     for (var i = 0; i < argv_.length; i++) {
-        globbedFiles = globbedFiles.concat(glob.sync(argv_[i]))
-    }
-    if(!globbedFiles) {
-        globbedFiles = argv_
+
+        let globResult = glob.sync(argv_[i])
+        
+        if(globResult.length < 1) {
+            // Glob found nothing. Just add this argument as-is.
+            globbedFiles.push(argv_[i])
+        }
+        else {
+            globbedFiles = globbedFiles.concat(globResult)
+        }
     }
     return globbedFiles
 }
--- test/glob-test.js
+++ test/glob-test.js
@@ -37,3 +37,40 @@ test('Glob still parses an actual file', function(t) {
     actual = result[0]
     t.equal(actual, expected, "Is result expected filename - Expected: " + expected + "; Actual: " + actual)
 })
+
+test('A combination of different inputs', function(t) {
+    t.plan(6)
+
+    let arg = ["test/glob-test.js", "test/*.js", "wibble/wibble123.example", "wibble/*.wibblet"]
+
+    // NOTE: Wibble-related paths do not exist. These should still be passed to the test runner, 
+    // in order to comply with the previous implementation.
+
+    let result = multi_tape.globArgs(arg)
+
+    let expected = true
+    let actual = Array.isArray(result)
+    t.equal(Array.isArray(result), true, "Is result array - Expected: " + expected + ", Actual: " + actual)
+    
+    // We're expecting 4 - 1 for each of the "legal" files and 2 for the wibble arguments, which will remain untouched.
+    // TODO: If the number of test files changes, this test will fail...
+    expected = 4
+    actual = result.length;
+    t.equal(actual, expected, "Number of .js files found by glob - Expected: " + expected + ", Actual: " + actual)
+
+    expected = "test/" + path.basename(__filename)
+    actual = result[0]
+    t.equal(actual, expected, "Is result expected filename - Expected: " + expected + "; Actual: " + actual)
+    
+    expected = "test/" + path.basename(__filename)
+    actual = result[1]
+    t.equal(actual, expected, "Is result expected filename - Expected: " + expected + "; Actual: " + actual)
+
+    expected = "wibble/wibble123.example"
+    actual = result[2]
+    t.equal(actual, expected, "Is result expected filename - Expected: " + expected + "; Actual: " + actual)
+
+    expected = "wibble/*.wibblet"
+    actual = result[3]
+    t.equal(actual, expected, "Is result expected filename - Expected: " + expected + "; Actual: " + actual)
+})
