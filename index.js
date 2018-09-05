a3350ac7bc5efb6e030f01a8f152a3065eb3fda1
Mattias Holmlund <mattias@holmlund.se>
2018-09-05 20:20:19 +0200

    Use prettier and reformat

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 
 'use strict'
 
-const argv = require('minimist')(process.argv.slice(2),{
+const argv = require('minimist')(process.argv.slice(2), {
     boolean: ['o'],
     default: {
-        p: 1
-    }
+        p: 1,
+    },
 })
 
 const parser = require('tap-parser')
@@ -21,11 +21,10 @@ let results = {}
 let exitCodes = {}
 
 let nodeArgs = []
-if(argv['node-arg']) {
-    if(Array.isArray(argv['node-arg'])) {
+if (argv['node-arg']) {
+    if (Array.isArray(argv['node-arg'])) {
         nodeArgs = argv['node-arg']
-    }
-    else {
+    } else {
         nodeArgs.push(argv['node-arg'])
     }
 }
@@ -34,14 +33,12 @@ if(argv['node-arg']) {
 function globArgs(argv_) {
     let globbedFiles = []
     for (var i = 0; i < argv_.length; i++) {
-
         let globResult = glob.sync(argv_[i])
-        
-        if(globResult.length < 1) {
+
+        if (globResult.length < 1) {
             // Glob found nothing. Just add this argument as-is.
             globbedFiles.push(argv_[i])
-        }
-        else {
+        } else {
             globbedFiles = globbedFiles.concat(globResult)
         }
     }
@@ -52,15 +49,16 @@ module.exports.globArgs = globArgs
 let files = globArgs(argv._).sort()
 
 // Start argv.p tests in parallel
-async.eachLimit(files, runTest, argv.p)
+async
+    .eachLimit(files, runTest, argv.p)
     .then(printSummary)
     .catch(console.dir) // eslint-disable-line no-console
 
 // Returns a promise that resolves whe the test has been run
 function runTest(filename) {
     let proc = spawn('node', nodeArgs.concat(filename))
-    let exited = new Promise( function(resolve) {
-        proc.on('exit', function (exitCode) {
+    let exited = new Promise(function(resolve) {
+        proc.on('exit', function(exitCode) {
             resolve(exitCode)
         })
     })
@@ -68,53 +66,54 @@ function runTest(filename) {
     let output
 
     let parsed = new Promise(function(resolve) {
-        let p = parser( resolve )
-        if(argv.p === 1) {
+        let p = parser(resolve)
+        if (argv.p === 1) {
             output = process.stdout
-        }
-        else {
+        } else {
             output = new streams.WritableStream()
         }
 
         output.write('\n#\n# ' + filename + '\n#\n')
 
-        if(argv.o) {
-            proc.stdout.pipe(tee(p, fs.createWriteStream(filename + '.tap') ))
+        if (argv.o) {
+            proc.stdout
+                .pipe(tee(p, fs.createWriteStream(filename + '.tap')))
                 .pipe(output)
-        }
-        else {
+        } else {
             proc.stdout.pipe(tee(p)).pipe(output)
         }
         proc.stderr.pipe(output)
     })
 
-    return Promise.all([exited, parsed]).then(function(values) {
-        exitCodes[filename] = values[0]
-        results[filename] = values[1]
-        if( argv.p > 1 ) {
-            console.log(output.toString()) // eslint-disable-line no-console
-        }
-
-    }).catch(console.dir) // eslint-disable-line no-console
+    return Promise.all([exited, parsed])
+        .then(function(values) {
+            exitCodes[filename] = values[0]
+            results[filename] = values[1]
+            if (argv.p > 1) {
+                console.log(output.toString()) // eslint-disable-line no-console
+            }
+        })
+        .catch(console.dir) // eslint-disable-line no-console
 }
 
 function printSummary() {
     let success = true
-    for(let file of Object.keys(results).sort()) {
-        if(exitCodes[file] !== 0) {
+    for (let file of Object.keys(results).sort()) {
+        if (exitCodes[file] !== 0) {
             success = false
             console.log(file + ' exited with error ' + exitCodes[file]) // eslint-disable-line no-console
-        }
-        else {
+        } else {
             let r = results[file]
-            if( !r.ok ) {
+            if (!r.ok) {
                 success = false
             }
-            console.log(file + (r.ok ? ' ok ' : ' fail ') + r.pass + '/' + r.count) // eslint-disable-line no-console
+            console.log(
+                file + (r.ok ? ' ok ' : ' fail ') + r.pass + '/' + r.count,
+            ) // eslint-disable-line no-console
         }
     }
 
-    if(!success) {
+    if (!success) {
         process.exit(1)
     }
 }
