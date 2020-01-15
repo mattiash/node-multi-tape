26173cecc90cd994ee3a77829c883cb7d8acbce6
Mattias Holmlund <mattias@holmlund.se>
2020-01-15 22:02:26 +0100

    Adapt to nyc changes

--- test/test-aaa-large-output.ts
+++ test/test-aaa-large-output.ts
@@ -1,3 +1,4 @@
+import 'source-map-support/register'
 console.log('# Test with long output')
 for (let n = 1; n < 10000; n++) {
     console.log('This test is very verbose...')
--- test/test-glob.ts
+++ test/test-glob.ts
@@ -1,3 +1,4 @@
+import 'source-map-support/register'
 import * as test from 'purple-tape'
 import { globArgs } from '../lib/glob'
 
