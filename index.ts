6e4d5924e40eb7be021e0c22d4981284b0e7cf74
Ian Goodacre <IanGoodacre3@gmail.com>
2026-08-10 07:39:46 +1200

    Sort failed tests to the end of the summary
    With many tests, this ensures all the failures don't scroll off the
    screen.

--- index.ts
+++ index.ts
@@ -436,9 +436,10 @@ async function run() {
 function printSummary() {
     let success = true
     if (!argv.q) {
-        console.log('')
+        console.log('test')
         for (const [file, res] of [...results.entries()].sort(
-            (a, b) => a[1].executionTime - b[1].executionTime
+            (a, b) => a[1].exitCode - b[1].exitCode ||
+                a[1].executionTime - b[1].executionTime
         )) {
             printTestResult(file, res)
             if (res.exitCode !== 0 || !res.result.ok) {
